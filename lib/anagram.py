27b4f0f9dd8d13549e037caa7016e743064e8440
Ricardo Guerra <RikkuG491@gmail.com>
2023-04-29 16:33:06 -0400

    Completed lab

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,18 @@
 # your code goes here!
+
+class Anagram:
+    
+    def __init__(self, word):
+        self.word = word
+
+    def match(self, list):
+        return [word for word in list if(self.is_anagram(word))]
+    
+    def is_anagram(self, word):
+        if(not len(word) == len(self.word)):
+            return False
+        
+        for letter in word:
+            if not letter in self.word:
+                return False
+        return True
